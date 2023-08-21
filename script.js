b178c129ece65eed0f18c0bde3020af7c7292c56
Yaroslav Popryho <70857282+poprygo@users.noreply.github.com>
2023-08-21 15:24:03 +0200

    Circle -> continueCircle

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Get references to HTML elements
 var video = document.getElementById('myVideo');
-var Circle = document.getElementById('Circle');
+var continueCircle = document.getElementById('continueCircle');
 var arrowContainer = document.getElementById('arrow-container');
 
 // Time in seconds when the video should pause
@@ -11,15 +11,15 @@ video.addEventListener('timeupdate', function pauseAtTime() {
     if (video.currentTime > pauseTime) {
         video.pause();
         video.removeEventListener('timeupdate', pauseAtTime); // Remove the listener once it's triggered
-        Circle.style.display = 'block'; // Show the continue circle
+        continueCircle.style.display = 'block'; // Show the continue circle
         arrowContainer.style.display = 'block'; // Show the arrow container
         arrowContainer.classList.add('show-arrow'); // Add class to start the arrow animation
     }
 });
 
 // Event listener to continue the video when the continue circle is clicked
-Circle.addEventListener('click', function() {
-    Circle.style.display = 'none'; // Hide the continue circle
+continueCircle.addEventListener('click', function() {
+    continueCircle.style.display = 'none'; // Hide the continue circle
     arrowContainer.style.display = 'none'; // Hide the arrow container
     arrowContainer.classList.remove('show-arrow'); // Remove class to stop the arrow animation
     video.play(); // Resume the video
